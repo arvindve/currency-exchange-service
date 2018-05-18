dd0da615e3a026decb278adaac1b8175fb5bfceb
arvindve <arvindcse.verma@gmail.com>
2018-05-18 08:27:05 +0800

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO EXCHANGE_VALUE(id,currency_from,currency_to,conversion_multiple,port)
+        VALUES (1000,'USD','INR',65,0);
+INSERT INTO EXCHANGE_VALUE(id,currency_from,currency_to,conversion_multiple,port)
+VALUES (1001,'EURO','INR',75,0);
+INSERT INTO EXCHANGE_VALUE(id,currency_from,currency_to,conversion_multiple,port)
+VALUES (1002,'HKD','INR',9,0);
